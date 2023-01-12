866457f03152a7a3e5aef4cd3e4f3e19f33c2c16
abhiram <abhiram.p317@gmail.com>
2023-01-12 07:58:30 +0000

    greeting code committed

--- Day01/second-java-project/src/main/java/com/abhiram/app/App.java
+++ Day01/second-java-project/src/main/java/com/abhiram/app/App.java
@@ -1,13 +1,15 @@
-package com.rama.app;
+package com.abhiram.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Greeting greeting=new Greeting();
+        String hello=greeting.sayHello();
+        System.out.println(hello);
+        String welcome=greeting.sayWelcome();
+        System.out.println(welcome);
+        String bye=greeting.sayBye();
+        System.out.println(bye);
     }
 }
--- Day01/second-java-project/src/test/java/com/abhiram/app/AppTest.java
+++ Day01/second-java-project/src/test/java/com/abhiram/app/AppTest.java
@@ -1,38 +1,32 @@
-package com.rama.app;
+package com.abhiram.app;
 
-import junit.framework.Test;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import junit.framework.Assert;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+
+
+
+public class AppTest extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public void testSayHello(){
+        String expectedResult= "Hello from greeting";
+        Greeting greeting =new Greeting();
+        String result= greeting.sayHello();
+        Assert.assertEquals(expectedResult,result);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public void testSayWelcome(){
+        String expectedResult= "Welcome from greeting";
+        Greeting greeting =new Greeting();
+        String result= greeting.sayWelcome();
+        Assert.assertEquals(expectedResult,result);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testSayBye(){
+        String expectedResult= "Bye from greeting";
+        Greeting greeting =new Greeting();
+        String result= greeting.sayBye();
+        Assert.assertEquals(expectedResult,result);
     }
 }
