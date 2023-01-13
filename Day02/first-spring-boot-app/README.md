5086759d04a76e4ada81a6ce7827889835999d6a
abhiram <abhiram.p317@gmail.com>
2023-01-13 06:19:00 +0000

    second spring-boot executed

---
+++ Day02/first-spring-boot-app/README.md
@@ -0,0 +1,11 @@
+#Create Springboot project
+
+https://start.spring.io/
+
+#Build project
+
+mvn clean install
+
+#Run the application
+
+mvn spring-boot:run
---
+++ Day02/second-spring-boot-app/src/test/java/com/abhiram/app/secondspringbootapp/SecondSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhiram.app.secondspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
