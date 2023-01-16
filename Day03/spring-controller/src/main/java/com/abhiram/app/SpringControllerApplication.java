b5849a84c68500a41cb0457c4bd8fa0bb5ef378f
abhiram <abhiram.p317@gmail.com>
2023-01-16 04:57:03 +0000

    spring controller task done

---
+++ Day03/spring-controller/src/main/java/com/abhiram/app/SpringControllerApplication.java
@@ -0,0 +1,13 @@
+package com.abhiram.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringControllerApplication.class, args);
+	}
+
+}
