d4f9f880358e9b794f8fdb6e022f0567cb584c21
abhiram <abhiram.p317@gmail.com>
2023-01-23 05:31:35 +0000

    Test cases added

--- Day06/patient-app/src/main/java/com/abhiram/app/controller/PatientController.java
+++ Day06/patient-app/src/main/java/com/abhiram/app/controller/PatientController.java
@@ -3,15 +3,19 @@ package com.abhiram.app.controller;
 import com.abhiram.app.model.Patient;
 import com.abhiram.app.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PatientController {
+    @Value("${app.name}")
+    String name;
     @Autowired
     PatientService patientService;
 
     @PostMapping("/patient/save")
     public Patient savePatient(@RequestBody Patient patient){
+        System.out.println(name);
         System.out.println(patient);
         return patient;
     }
