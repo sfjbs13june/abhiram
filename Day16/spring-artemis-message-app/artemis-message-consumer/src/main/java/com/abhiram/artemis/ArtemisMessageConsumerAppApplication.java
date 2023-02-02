5252f8f4709e6453ed97e3d84d5514647350f71d
abhiram <abhiram.p317@gmail.com>
2023-02-02 07:42:13 +0000

    Assignment done

---
+++ Day16/spring-artemis-message-app/artemis-message-consumer/src/main/java/com/abhiram/artemis/ArtemisMessageConsumerAppApplication.java
@@ -0,0 +1,13 @@
+package com.abhiram.artemis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemisMessageConsumerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtemisMessageConsumerAppApplication.class, args);
+	}
+
+}
